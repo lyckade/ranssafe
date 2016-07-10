b5f1ddeac8018979f8e299efa7f1946935b2825d
as27 <andreas@apxp.info>
2016-07-10 20:29:09 +0200

    distsync tests prepared

--- distsync/distsync.go
+++ distsync/distsync.go
@@ -9,10 +9,23 @@ import (
 // Distsyncer interface is used for syncing files between
 // server and client. The interface lets the implementation
 // of the protocol open.
+// The sync process is very simple. First the files which have
+// to be synced will be returned by GetSrcFileInfo(). All files
+// inside this slice are going to be synced. Patterns for skipping
+// folders or files have to be includes inside that method.
+// The next step gets all the fileInfos from the dist location.
+// If the local file is newer it will be pushed, if not the file
+// is going to be loaded from the dist path.
 type Distsyncer interface {
 	GetSrcFileInfo() []fileinfo.File
 	GetDistFileInfo() []fileinfo.File
-	SkipFile(string) bool
+	//SkipFile(string) bool
 	PushFile(string) error
 	GetFile(string) (io.Writer, error)
+}
+
+// Distsync uses the Distsyncer interface to sync between different
+// locations.
+func Distsync(ds Distsyncer) error {
+	return nil
 }
--- distsync/distsync_test.go
+++ distsync/distsync_test.go
@@ -1,27 +1,69 @@
 package distsync
 
-import "github.com/as27/ranssafe/fileinfo"
+import (
+	"io"
+	"reflect"
+	"testing"
 
-type testDistsyncer struct{}
+	"github.com/as27/ranssafe/fileinfo"
+)
+
+type testDistsyncer struct {
+	pushFiles []string
+	getFiles  []string
+}
 
 func (ds *testDistsyncer) GetSrcFileInfo() []fileinfo.File {
-	return nil
+	testFiles := []fileinfo.File{
+		{"a/b/c/file1", 2016010131000000},
+		{"a/b/c/sametimestamp", 2016010131000000},
+		{"a/b/newer", 2016010131000000},
+		{"a/b/c/older", 2015010131000000},
+	}
+	return testFiles
 }
 
 func (ds *testDistsyncer) GetDistFileInfo() []fileinfo.File {
-	return nil
+	testFiles := []fileinfo.File{
+		{"a/b/c/file2", 2016010131000000},
+		{"a/b/c/sametimestamp", 2016010131000000},
+		{"a/b/newer", 2015010131000000},
+		{"a/b/c/older", 2016010131000000},
+	}
+	return testFiles
 }
 
-func (ds *testDistsyncer) SkipFile(fp string) bool {
+/*func (ds *testDistsyncer) SkipFile(fp string) bool {
 	return false
-}
+}*/
 
 func (ds *testDistsyncer) PushFile(fp string) error {
+	ds.pushFiles = append(ds.pushFiles, fp)
 	return nil
 }
 
-func (ds *testDistsyncer) GetFile(fp string) error {
-	return nil
+func (ds *testDistsyncer) GetFile(fp string) (io.Writer, error) {
+	ds.getFiles = append(ds.getFiles, fp)
+	return nil, nil
 }
 
+// Check the correct implementation
 var _ Distsyncer = &testDistsyncer{}
+
+func DistsyncTest(t *testing.T) {
+	ds := &testDistsyncer{}
+	Distsync(ds)
+	expectPush := []string{
+		"a/b/c/file1",
+		"a/b/newer",
+	}
+	if reflect.DeepEqual(ds.pushFiles, expectPush) {
+		t.Fatal("Push files not correct")
+	}
+	expectGet := []string{
+		"a/b/c/older",
+	}
+	if reflect.DeepEqual(ds.getFiles, expectGet) {
+		t.Fatal("Get files not correct")
+	}
+}
